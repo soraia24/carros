73cbe2b58e5d0ed870d5b2c15ff3133a8267624a
Arturgr21 <artur.queiroz.089@ufrn.edu.br>
2025-11-29 15:49:40 -0300

    admin criado e verificaradmin nas rotas

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.get("/", autenticarToken, verificarAdmin,  usuariosController.todosUsuarios);
 router.get("/:id", autenticarToken, verificarAdmin, usuariosController.buscar);
-router.post('/cadastrar', autenticarTokenOptional, validarNovoUsuario, usuariosController.novoUsuario);
+router.post('/cadastrar', autenticarTokenOptional, validarNovoUsuario, verificarAdmin, usuariosController.novoUsuario);
 router.post('/login', validarLogin, usuariosController.login);
 router.put('/:id', autenticarToken, verificarProprioUsuario, validarNovoUsuario, usuariosController.atualizar);
 router.delete('/:id', autenticarToken, verificarProprioUsuario, usuariosController.deletar);
